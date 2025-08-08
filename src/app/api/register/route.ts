1b4cb7d50da3de4db4671d4db94d448f847fbb1a
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-08-09 01:55:46 +0600

    added user post api

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,34 @@
+import { dbConnect } from "@/lib/dbConnect";
+import { User } from "@/models/user";
+
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
+
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: "All fields are required." }, { status: 400 });
+    }
+
+    await dbConnect();
+
+    // ✅ Check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json({ error: "Email already registered." }, { status: 409 });
+    }
+
+    // ✅ Save new user via model
+    const newUser = await User.create({ name, email, password });
+
+    return NextResponse.json(
+      { success: true, insertedId: newUser._id },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("❌ Error in registration:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+import  { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// prevent model overwrite in dev
+export const User = models.User || model("User", userSchema);

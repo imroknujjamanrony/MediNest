f1d038a28d493a4c52b4a8f66a1b52527a12de07
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-08-12 23:15:54 +0600

    added sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,21 @@
-// import DashboardAside from "@/components/dashboard/DashboardAside";
+// app/dashboard/layout.tsx
+import DashboardAside from "@/components/dashboard/DashboardAside";
 import DashboardTop from "@/components/dashboard/DashboardTop";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
-export default function DashboardLayout() {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <SidebarProvider>
-      <div>
-        <h1>hello from DashboardLayout</h1>
-        <DashboardTop></DashboardTop>
+      <div className="flex min-h-screen bg-gray-50">
+        <DashboardAside></DashboardAside>
+        <div className="flex flex-1 flex-col">
+          <DashboardTop></DashboardTop>
+          <main className="flex-1 p-4">{children}</main>
+        </div>
       </div>
     </SidebarProvider>
   );

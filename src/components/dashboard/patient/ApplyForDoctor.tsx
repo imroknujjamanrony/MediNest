d353245bfc82499ca60ff21eadb943d59d6149b1
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-08-15 21:06:02 +0600

    validate form using zod

--- src/components/dashboard/patient/ApplyForDoctor.tsx
+++ src/components/dashboard/patient/ApplyForDoctor.tsx
@@ -20,38 +20,12 @@ import {
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+
+import {
+  doctorFormSchema,
+  DoctorFormType,
+} from "@/lib/validation/doctorFormSchema";
 // import { useDispatch } from "react-redux";
-
-// ✅ Validation Schema
-const doctorFormSchema = z.object({
-  fullName: z.string().min(3, "Full name is required"),
-  email: z.string().email("Invalid email"),
-  phone: z.string().min(6, "Phone number required"),
-  dob: z.string().nonempty("Date of birth required"),
-  specialization: z.string().nonempty("Select specialization"),
-  licenseNumber: z.string().nonempty("License number required"),
-  licenseIssueDate: z.string().nonempty("Issue date required"),
-  licenseExpiryDate: z.string().nonempty("Expiry date required"),
-  experience: z.string().nonempty("Experience required"),
-  workplace: z.string().nonempty("Workplace name required"),
-  nidUrl: z.string().url("Enter valid URL"),
-  degreeUrl: z.string().url("Enter valid URL"),
-  bmdcUrl: z.string().url("Enter valid URL"),
-  profileUrl: z.string().url("Enter valid URL"),
-  fee: z.string().nonempty("Fee required"),
-  schedule: z
-    .array(
-      z.object({
-        day: z.string().nonempty(),
-        startTime: z.string().nonempty(),
-        endTime: z.string().nonempty(),
-      })
-    )
-    .min(1, "At least one schedule is required"),
-});
-
-type DoctorFormType = z.infer<typeof doctorFormSchema>;
 
 export default function DoctorApplicationForm() {
   //   const dispatch = useDispatch();
@@ -85,7 +59,7 @@ export default function DoctorApplicationForm() {
 
   const onSubmit = (data: DoctorFormType) => {
     console.log("✅ Form Data:", data);
-    // dispatch(addDoctorApplication(data)); // Redux এ পাঠানো
+    // dispatch(addDoctorApplication(data));
     form.reset();
   };
 
--- src/lib/validation/doctorFormSchema.ts
+++ src/lib/validation/doctorFormSchema.ts
@@ -1,7 +1,30 @@
-export const doctorFormSchema=z.object
-const rony={
-    name:'hello rony',
-    age:22,
-    address:'dhaka',
-    city:'dhaka'
-}
+import z from "zod";
+
+export const doctorFormSchema=z.object({
+    fullName: z.string().min(3, "Full name is required"),
+      email: z.string().email("Invalid email"),
+      phone: z.string().min(6, "Phone number required"),
+      dob: z.string().nonempty("Date of birth required"),
+      specialization: z.string().nonempty("Select specialization"),
+      licenseNumber: z.string().nonempty("License number required"),
+      licenseIssueDate: z.string().nonempty("Issue date required"),
+      licenseExpiryDate: z.string().nonempty("Expiry date required"),
+      experience: z.string().nonempty("Experience required"),
+      workplace: z.string().nonempty("Workplace name required"),
+      nidUrl: z.string().url("Enter valid URL"),
+      degreeUrl: z.string().url("Enter valid URL"),
+      bmdcUrl: z.string().url("Enter valid URL"),
+      profileUrl: z.string().url("Enter valid URL"),
+      fee: z.string().nonempty("Fee required"),
+      schedule: z
+        .array(
+          z.object({
+            day: z.string().nonempty(),
+            startTime: z.string().nonempty(),
+            endTime: z.string().nonempty(),
+          })
+        )
+        .min(1, "At least one schedule is required"),
+})
+
+export type DoctorFormType = z.infer<typeof doctorFormSchema>;

a374eb3d871c1a50682671c32e75688812d9e9ef
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-08-16 21:51:46 +0600

    added some changes in the validation

--- src/components/dashboard/patient/ApplyForDoctor.tsx
+++ src/components/dashboard/patient/ApplyForDoctor.tsx
@@ -61,8 +61,9 @@ export default function DoctorApplicationForm() {
     console.log("✅ Form Data:", data);
     // dispatch(addDoctorApplication(data));
     form.reset();
+    form.reset();
   };
-
+  //changes for commit
   return (
     <div className="max-w-4xl mx-auto p-4">
       <Card>
